4db77a89720c85b89352d24bf897236ea456d91e
Simon <simon@wickman.in>
2025-08-16 17:06:35 +0300

    Added functions for subtract, multiply and divide

--- calculator.js
+++ calculator.js
@@ -2,3 +2,18 @@ function add(num1, num2) {
   const answer = num1 + num2;
   return(answer);
 }
+
+function subtract(num1, num2) {
+  const answer = num1 - num2;
+  return(answer);
+}
+
+function multiply(num1, num2) {
+  const answer = num1 * num2;
+  return(answer);
+}
+
+function divide(num1, num2) {
+  const answer = num1 / num2;
+  return(answer);
+}
