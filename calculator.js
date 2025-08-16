b53634c769f6631467d538a6ac7fa62a60278b04
Simon <simon@wickman.in>
2025-08-16 17:14:20 +0300

    Added a function "operate"

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
+let num1;
+let operator;
+let num2;
+
+
+
 function add(num1, num2) {
   const answer = num1 + num2;
   return(answer);
@@ -16,4 +22,23 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   const answer = num1 / num2;
   return(answer);
+}
+
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return(add(num1, num2));
+  }
+  
+  if (operator === "-") {
+    return(subtract(num1, num2));
+  }
+  
+  if (operator === "*") {
+    return(multiply(num1, num2));
+  }
+  
+  if (operator === "/") {
+    return(divide(num1, num2));
+  }
 }
